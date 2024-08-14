7ae60e097c2328981a8fbee8605477960afe9609
Blueboltz <vincentmariscotescleofe@gmail.com>
2024-08-14 20:23:48 +0800

    using eas build

---
+++ app/(tabs)/laboratory.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Laboratory = () => {
+  return (
+    <View>
+      <Text>Laboratory</Text>
+    </View>
+  );
+};
+
+export default Laboratory;
+
+const styles = StyleSheet.create({});
